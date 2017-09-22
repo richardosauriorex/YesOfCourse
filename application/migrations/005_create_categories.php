29d054514f30cb827ae967a8246456129c4ba19c
richardosauriorex <richarm_zero@hotmail.com>
2017-09-21 21:22:38 -0500

    add migration file to create categories table

---
+++ application/migrations/005_create_categories.php
@@ -0,0 +1,34 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_Create_categories extends CI_Migration {
+
+	public function __construct()
+	{
+		$this->load->dbforge();
+	}
+
+	public function up() {
+		$this->dbforge->add_field(array(
+			'category_id' => array(
+				'type' => 'INT',
+				'unsigned' => TRUE,
+				'auto_increment' => TRUE
+			),
+			'category_description' => array(
+				'type' => 'VARCHAR',
+				'constraint' => '255'
+			)
+		));
+		$this->dbforge->add_key('cateogry_id', TRUE);
+        $this->dbforge->create_table('categories');
+	}
+
+	public function down() {
+		$this->dbforge->drop_table('categories');
+	}
+
+}
+
+/* End of file 005_create_categories.php */
+/* Location: ./application/migrations/005_create_categories.php */
