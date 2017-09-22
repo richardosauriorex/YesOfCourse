6c463159a735630d73f568ce83352339d422beaa
richardosauriorex <richarm_zero@hotmail.com>
2017-09-21 23:50:08 -0500

    add migration file to create inscriptions table

---
+++ application/migrations/009_create_inscriptions.php
@@ -0,0 +1,45 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_Create_inscriptions extends CI_Migration {
+
+	public function __construct()
+	{
+		$this->load->dbforge();
+	}
+
+	public function up() {
+		$this->dbforge->add_field(array(
+				'inscription_id' => array(
+					'type' => 'INT',
+					'default' => TRUE,
+					'unsigned' => TRUE,
+					'auto_increment' => TRUE
+				),
+				'user_id' => array(
+					'type' => 'INT',
+					'unsigned' => TRUE,
+					'default' => TRUE
+				),
+				'course_id' => array(
+					'type' => 'INT',
+					'default' => TRUE,
+					'unsigned' => TRUE
+				),
+				'status_id' => array(
+			        'type' => 'INT',
+					'unsigned' => TRUE
+				)
+		));
+		$this->dbforge->add_key('inscription_id', TRUE);
+		$this->dbforge->create_table('inscriptions');
+	}
+
+	public function down() {
+		$this->dbforge->drop_table('inscriptions');
+	}
+
+}
+
+/* End of file 009_create_inscriptions.php */
+/* Location: ./application/migrations/009_create_inscriptions.php */
