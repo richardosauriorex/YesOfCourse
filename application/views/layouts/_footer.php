43bf681187bd8d69eb238a782c1ae11d7ff9c0c1
richardosauriorex <richarm_zero@hotmail.com>
2017-10-18 21:23:58 -0500

    Include custom.js and custom.css

--- application/views/layouts/_footer.php
+++ application/views/layouts/_footer.php
@@ -1,3 +1,3 @@
-<script src="<?php echo base_url()."vendor/jquery/dist/jquery.min.js";?>" ></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
 <script src="<?php echo base_url()."vendor/bootstrap/dist/js/bootstrap.min.js";?>"></script>
+<script src="<?php echo base_url()."assets/js/custom.js";?>"></script>
