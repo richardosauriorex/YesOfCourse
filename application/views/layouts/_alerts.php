1df20980ef3289be9a818b7bb9207c5f21472fb9
richardosauriorex <richarm_zero@hotmail.com>
2017-10-18 21:26:40 -0500

    Modify _alerts.php for add alert element

---
+++ application/views/layouts/_alerts.php
@@ -0,0 +1,6 @@
+<div id="alert" class="alert alert-dismissible fade" role="alert">
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+  <strong id="title"></strong><div id="msg"></div>
+</div>
