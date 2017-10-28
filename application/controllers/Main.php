1261c6cc2da6952ac74f39ab6221f7425ec4ff13
richardosauriorex <richarm_zero@hotmail.com>
2017-10-27 19:48:31 -0500

    Add functionality to register user

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -11,7 +11,7 @@ class Main extends CI_Controller {
 	public function up()
 	{
 		$this->load->library('migration');
-		if ($this->migration->version(12) === FALSE)
+		if ($this->migration->version(13) === FALSE)
 			{
             	show_error($this->migration->error_string());
             }
@@ -28,6 +28,39 @@ class Main extends CI_Controller {
 	public function register()
 	{
 		$this->custom->layouts('main/register');
+	}
+
+	public function regUser()
+	{
+		$this->form_validation->set_rules('firstName', 'Nombre(s)', 'trim|required');
+		$this->form_validation->set_rules('lastName', 'Apellido paterno', 'trim|required');
+		$this->form_validation->set_rules('lastMotherName', 'Apellido materno', 'trim|required');
+		$this->form_validation->set_rules('userEmail', 'Correo electrónico', 'trim|required|is_unique[users.user_email]');
+		$this->form_validation->set_rules('password', 'Contraseña', 'trim|required|min_length[8]');
+		if ($this->form_validation->run() == TRUE) {
+			$values=[
+				'user_email' => $this->input->post('userEmail', TRUE),
+				'first_name' => $this->input->post('firstName', TRUE),
+				'last_name' => $this->input->post('lastName', TRUE),
+				'last_mother_name' => $this->input->post('lastMotherName', TRUE),
+				'user_email' => $this->input->post('userEmail', TRUE),
+				'password' => $this->encryption->encrypt($this->input->post('password', TRUE)),
+				'authentication_code' => random_string('alnum', 16),
+				'status_id' => 'user0'
+			];
+			/*$this->users->insert($values);
+			$this->custom->sendEmail($emailDestiny, $subject, $message);
+			*/
+			$title = '<i class="fa fa-thumb-up"></i>YesOfCourse';
+			$msg = 'Se ha enviado un mensaje a su correo electrónico para continuar con su registro.';
+			$bColor = 'alert-success';
+			$this->custom->alert($title, $msg, $bColor);
+		} else {
+			$title = '<i class="fa fa-thumb-down"></i>YesOfCourse';
+			$msg = validation_errors('<br>');
+			$bColor = 'alert-danger';
+			$this->custom->alert($title, $msg, $bColor);
+		}
 	}
 
 	public function validate($user_id = '', $code = '')
---
+++ application/views/main/register.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Registro</title>
+</head>
+<body>
+	<div class="container">
+		<div class="jumbotron bg-dark text-white">
+			<div class="container">
+				<h2 class="text-center">Registrar</h2>
+				<?= form_open('main/regUser', ['id' => 'register', 'class' => 'container was-validated']); ?>
+				<div class="form-group">
+					<label>Nombre(s):</label>
+					<input type="text" class="form-control" id="nFirstName" required>
+				</div>
+				<div class="form-group">
+					<label>Apellido Paterno:</label>
+					<input type="text" class="form-control" id="nLastName" required>
+				</div>
+				<div class="form-group">
+					<label>Apellido Materno:</label>
+					<input type="text" class="form-control" id="nLastMotherName" required>
+				</div>
+				<div class="form-group">
+					<label>Correo electrónico:</label>
+					<input type="email" class="form-control" id="nUserEmail" required>
+				</div>
+				<div class="form-group">
+					<label>Password:</label>
+					<input type="password" class="form-control" id="nPassword" required>
+				</div>
+				<div class="text-center">
+					<button type="reset" class="btn bg-warning text-white">Borrar</button>
+					<button type="submit" class="btn bg-success text-white">Enviar</button>
+				</div>
+				<?= form_close(); ?>
+			</div>
+		</div>
+	</div>
+	<script>
+		$(function() {
+			$('#register').on('submit', function(event) {
+				event.preventDefault();
+				var data = {
+					firstName : $('#nFirstName').val(),
+					lastName : $('#nLastName').val(),
+					lastMotherName : $('#nLastMotherName').val(),
+					userEmail : $('#nUserEmail').val(),
+					password : $('#nPassword').val()
+				}
+				var url = $('#register').attr('action');
+				request_ajax(url, data);
+			});
+		});
+	</script>
+</body>
+</html>
